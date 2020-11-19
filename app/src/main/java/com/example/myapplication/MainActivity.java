e6621d019b75dd5c8e2c3291a40723f6796d7ba0
hneymeyer <70923749+hneymeyer@users.noreply.github.com>
2020-11-19 10:42:29 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //--- text view---
+        TextView txtView = (TextView) findViewById(R.id.text_id);
+    }
+}
